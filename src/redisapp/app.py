235677c2e2664dc254a6874b29349a45307eef21
Alexa Duarte <alexa-2095@hotmail.com>
2018-12-06 02:27:00 -0600

    Pruebas de promedio

--- src/redisapp/app.py
+++ src/redisapp/app.py
@@ -3,6 +3,7 @@
 import redis
 import string
 import random
+import time
 
 class App(object):
     r""" Clase que implementa la lógica de la aplicación.
@@ -69,3 +70,91 @@ class App(object):
 
 
         return top_products
+
+    def set_average(self):
+        products = self.redis.hgetall('reviews.by.product')
+        cont = 0
+
+        for product_id, reviews_id in products.iteritems():
+            #print
+            #"El product_id es {:s}".format(product_id)
+
+            array = reviews_id.split(" ")
+            total_reviews = len(array)
+            #print
+            #"El total de review_id es {:d}".format(total_reviews)
+
+            for i in array:
+            #    print
+            #    "El review_id es {:s}".format(i)
+                cont += float(self.redis.hmget('star.rating.by.review', i)[0])
+
+            average = cont / total_reviews
+
+           # print
+           # "El average es {:f}\n".format(average)
+            self.redis.zadd('products.by.average', product_id, average)
+
+            if self.redis.hexists('products.by.average.hash', average):
+                existent_product = self.redis.hmget('products.by.average.hash', average)
+                existent_product[0] = existent_product[0] + ' ' + product_id
+                self.redis.hset('products.by.average.hash', average, existent_product[0])
+            else:
+                self.redis.hset('products.by.average.hash', average, product_id)
+
+            cont = 0
+
+    def get_average_from_zset(self, n):
+        products = self.redis.zrevrange('products.by.average', 0, n)
+
+       # print "Los {:s} mejores promedios son: ".format(str(n))
+
+        for i in products:
+            title = self.redis.hmget('product.title.by.product.id', i)
+           # print title[0]
+
+	
+	def get_average_from_zset(self, n):
+        products = self.redis.zrevrange('products.by.average', 0, n)
+
+       # print "Los {:s} mejores promedios son: ".format(str(n))
+
+        for i in products:
+            title = self.redis.hmget('product.title.by.product.id', i)
+           # print title[0]
+
+    def get_average_from_hash(self,n):
+       # print "Los {:s} mejores promedios son: ".format(str(n))
+
+        averages = self.redis.hkeys('products.by.average.hash')
+        cont = 0
+        for i in averages:
+            products = self.redis.hmget('products.by.average.hash', i)[0].split(' ')
+            for j in products:
+                if(cont < n):
+                    title = self.redis.hmget('product.title.by.product.id', j)
+                  #  print title[0]
+
+                    cont += 1
+                    continue
+                else:
+                    break
+
+    def run_querys(self):
+        cont = 0
+
+        for i in range(100, 201):
+            start_time = time.time()
+            self.get_average_from_zset(i)
+            cont += time.time() - start_time
+
+        print "Tiempo de duración zset {:f}".format((cont/100)*100)
+
+        cont = 0
+
+        for i in range(100, 201):
+            start_time = time.time()
+            self.get_average_from_hash(i)
+            cont += time.time() - start_time
+
+        print "Tiempo de duración hash {:f}".format((cont/100)*100)
