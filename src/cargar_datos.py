ca81f1a2c5a66c200bf89a3408c36e65bb2ccc52
Daniel <danidiaza@gmail.com>
2018-12-06 10:44:01 -0600

    Agregar método para procesar csv mejorado

--- src/cargar_datos.py
+++ src/cargar_datos.py
@@ -4,6 +4,51 @@
 import sys
 import redis
 import string
+import csv
+
+
+csv.register_dialect('athena', delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+
+def procesar_csv(file, host='localhost', port=6379):
+
+    con = redis.Redis(host, port)
+    csv_reader = csv.DictReader(file, dialect='athena')
+    count = 0
+
+    for tupla in csv_reader:
+
+        review_id = tupla.pop('review_id')
+
+        con.hmset('review:' + review_id, tupla)
+        
+        product_id = tupla.pop('product_id')
+        product_title = tupla.pop('product_title')
+        star_rating = tupla.pop('star_rating')
+
+        con.zadd('reviews:marketplace:' + tupla['marketplace'], review_id, float(tupla['review_date']))
+
+        if con.hexists("reviews.by.product", product_id):
+            existent_id = con.hget("reviews.by.product", product_id)
+            existent_id = existent_id + ' ' + review_id
+            con.hset('reviews.by.product', product_id, existent_id)
+        else:
+            con.hset('reviews.by.product', product_id, review_id)
+
+        con.hset('product.title.by.product.id', product_id, product_title)
+
+        con.hset('star.rating.by.review', review_id, star_rating)
+        count += 1
+
+        if count%10000 == 0:
+            print "{:d} líneas procesadas...".format(count)
+
+
+    print "Se leyeron {:d} del archivo {:s}".format(count, file.name)
+    print "Formato:"
+    print csv_reader.fieldnames
+
+    return
 
 def procesar(file, host='localhost', port=6379):
     """ Cargar datos en los mapas "review:<valor>" y en los zsets "reviews:marketplace:<valor>"
@@ -29,11 +74,13 @@ def procesar(file, host='localhost', port=6379):
 
         # Insertar los datos en review:<id>
         review_id = tupla.pop('review_id')
+
+        con.hmset('review:' + review_id, tupla)
+
         product_id = tupla.pop('product_id')
         product_title = tupla.pop('product_title')
         star_rating = tupla.pop('star_rating')
 
-        con.hmset('review:' + review_id, tupla)
 
         con.zadd('reviews:marketplace:' + tupla['marketplace'], review_id, float(tupla['review_date']))
 
@@ -47,13 +94,14 @@ def procesar(file, host='localhost', port=6379):
         con.hset('product.title.by.product.id', product_id, product_title)
 
         con.hset('star.rating.by.review', review_id, star_rating)
-
         count += 1
 
         if count%10000 == 0:
             print "{:d} líneas procesadas...".format(count)
 
     print "Se leyeron {:d} del archivo {:s}".format(count, file.name)
+
+    return
 
 def main():
     if len(sys.argv) < 2:
@@ -64,7 +112,7 @@ def main():
 
     for path in sys.argv[1:]:
         with open(path, mode='r') as file:
-            procesar(file)
+            procesar_csv(file)
 
 
 
