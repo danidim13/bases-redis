f37b876a8185aee549099a49464403676d2aab90
Daniel <danidiaza@gmail.com>
2018-12-06 12:07:38 -0600

    Clase Tester para diseñar pruebas

--- src/main.py
+++ src/main.py
@@ -4,15 +4,16 @@
 import sys
 
 from redisapp.app import App
+from redisapp.tester import Tester
 
 def main():
 
-    clase = App()
+    aplicacion = App()
+    tester = Tester(aplicacion)
 
-    print 'Hola mundo'
-    for i in xrange(len(sys.argv)):
-        print sys.argv[i]
+    print 'prueba para 100 repeticiones de tendencias_v1'
+
+    tester.test_function(50, 'tendencias_v1', marketplace='FR', n='5', fecha_ini=13000, fecha_fin=15000)
 
 if __name__ == "__main__":
     main()
-
---
+++ src/redisapp/tester.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import redis
+import random
+import time
+import exceptions
+from app import App
+import numpy as np
+
+class Tester(object):
+
+    def __init__(self, app=None):
+        if app is None:
+            self.app = App()
+        else:
+            self.app = app
+
+    def test_function(self, repetitions, func_name, *args, **kwargs):
+        r"""
+        Args:
+            repetitions (int): Cantidad de repeticiones para la prueba.
+            func_name (string): Nombre de la función que se va a probar.
+
+        Returns:
+            np.array: Retorna un arreglo con el tiempo que tomó cada repetición.
+
+        Example:
+            >>> probador = Tester()
+            >>> resultado = probador.tests_function(100, 'tendencias_v1', marketplace='FR', n=5, fecha_ini=13000, fecha_fin=14000)
+        """
+
+        func = None
+        try:
+            func = getattr(self.app, func_name)
+        except Exception as e:
+            print "No existe el método " + func_name
+            raise e
+
+
+        loop_iter = 1
+        loop_time = None
+        while True:
+            start = time.clock()
+            for i in xrange(loop_iter): func(*args, **kwargs)
+            end = time.clock()
+
+            loop_time = end-start
+            if loop_time < 0.2:
+                loop_iter = loop_iter*5
+            else:
+                break;
+
+        print "Using {:d} loop_iter".format(loop_iter)
+
+        # imprimir un mensaje informativo cada 15 segundos
+        update_freq = 15.0
+        approx_loops_mod = int( update_freq * loop_iter / loop_time)
+
+        results = np.zeros(repetitions)
+        for j in xrange(repetitions):
+
+            start = time.clock()
+            for i in xrange(loop_iter): self.app.tendencias_v1(*args, **kwargs)
+            end = time.clock()
+            total = (end - start)/loop_iter
+
+            results[j] = total
+
+            if j%approx_loops_mod == 0:
+                print 'Repeticion {:d}...'.format(j)
+
+        min = np.min(results)
+        avg = np.mean(results)
+        max = np.max(results)
+        print 'Mínimo: {:f}, promedio: {:f}, máximo: {:f}'.format(min, avg, max)
+        return results
